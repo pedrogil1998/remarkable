c71281b87db3625166439d19046a6c896e6db0c4
Pedro <pedrogilmoura27@gmail.com>
2023-12-07 12:06:13 +0000

    MA25 - deploy bugfix2

--- src/components/Partner/PartnerData.js
+++ src/components/Partner/PartnerData.js
@@ -1,86 +1,89 @@
+import civilworks from "../../assets/Parceiros/civilworks.jpg";
+import flavio from "../../assets/Parceiros/flavio.jpg";
+
 export const PartnerData = [
   {
     id: 1,
     title: "Civilworks",
     type: "Engenharia",
-    image: "/remarkable/assets/Parceiros/civilworks.jpg",
+    image: civilworks,
   },
   {
     id: 2,
     title: "Flávio Magalhães Fisioterapeuta",
     type: "Saúde",
-    image: "/remarkable/assets/Parceiros/flavio.jpg",
+    image: flavio,
   },
   {
     id: 3,
     title: "Civilworks",
     type: "Engenharia",
-    image: "/remarkable/assets/Parceiros/civilworks.jpg",
+    image: civilworks,
   },
   {
     id: 4,
     title: "Civilworks",
     type: "Engenharia",
-    image: "/remarkable/assets/Parceiros/civilworks.jpg",
+    image: civilworks,
   },
   {
     id: 5,
     title: "Civilworks",
     type: "Engenharia",
-    image: "/remarkable/assets/Parceiros/civilworks.jpg",
+    image: civilworks,
   },
   {
     id: 6,
     title: "Civilworks",
     type: "Engenharia",
-    image: "/remarkable/assets/Parceiros/civilworks.jpg",
+    image: civilworks,
   },
   {
     id: 7,
     title: "Civilworks",
     type: "Engenharia",
-    image: "/remarkable/assets/Parceiros/civilworks.jpg",
+    image: civilworks,
   },
   {
     id: 8,
     title: "Flávio Magalhães Fisioterapeuta",
     type: "Saúde",
-    image: "/remarkable/assets/Parceiros/flavio.jpg",
+    image: flavio,
   },
   {
     id: 9,
     title: "Flávio Magalhães Fisioterapeuta",
     type: "Saúde",
-    image: "/remarkable/assets/Parceiros/flavio.jpg",
+    image: flavio,
   },
   {
     id: 10,
     title: "Flávio Magalhães Fisioterapeuta",
     type: "Saúde",
-    image: "/remarkable/assets/Parceiros/flavio.jpg",
+    image: flavio,
   },
   {
     id: 11,
     title: "Flávio Magalhães Fisioterapeuta",
     type: "Saúde",
-    image: "/remarkable/assets/Parceiros/flavio.jpg",
+    image: flavio,
   },
   {
     id: 12,
     title: "Flávio Magalhães Fisioterapeuta",
     type: "Saúde",
-    image: "/remarkable/assets/Parceiros/flavio.jpg",
+    image: flavio,
   },
   {
     id: 13,
     title: "Flávio Magalhães Fisioterapeuta",
     type: "Saúde",
-    image: "/remarkable/assets/Parceiros/flavio.jpg",
+    image: flavio,
   },
   {
     id: 14,
     title: "Flávio Magalhães Fisioterapeuta",
     type: "Saúde",
-    image: "/remarkable/assets/Parceiros/flavio.jpg",
+    image: flavio,
   },
 ];
