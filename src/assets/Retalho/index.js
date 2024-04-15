403b3ebb325c2f8264e3418717515c10398b6193
pedromoura <pedrogilmoura27@gmail.com>
2024-04-15 17:06:41 +0100

    MA61 - added images to setor automóvel

--- src/assets/Retalho/index.js
+++ src/assets/Retalho/index.js
@@ -4,6 +4,20 @@ import retalho3 from "./retalho3.jpg";
 import retalho4 from "./retalho4.jpg";
 import retalho5 from "./retalho5.jpg";
 import retalho6 from "./retalho6.jpg";
+import retalho7 from "./retalho7.jpg";
+import retalho8 from "./retalho8.jpg";
+import retalho9 from "./retalho9.jpg";
+import retalho10 from "./retalho10.jpg";
+import retalho11 from "./retalho11.jpg";
+import retalho12 from "./retalho12.jpg";
+import retalho13 from "./retalho13.jpg";
+import retalho14 from "./retalho14.jpg";
+import retalho15 from "./retalho15.jpg";
+import retalho16 from "./retalho16.jpg";
+import retalho17 from "./retalho17.jpg";
+import retalho18 from "./retalho18.jpg";
+import retalho19 from "./retalho19.jpg";
+
 const imagesRet = {
   retalho1,
   retalho2,
@@ -11,5 +25,19 @@ const imagesRet = {
   retalho4,
   retalho5,
   retalho6,
+  retalho7,
+  retalho8,
+  retalho9,
+  retalho10,
+  retalho11,
+  retalho12,
+  retalho13,
+  retalho14,
+  retalho15,
+  retalho16,
+  retalho17,
+  retalho18,
+  retalho19,
 };
+
 export default imagesRet;
